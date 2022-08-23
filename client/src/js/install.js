d0c5a8c9c09936594765e3e51d28041fef15af26
Dr Dyl <dylkreisman@gmail.com>
2022-08-22 22:21:35 -0500

    added stalewhilerevalidate

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,11 +8,12 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-const promptEvent = window.deferredPrompt;
-if (!promptEvent) {
-    return
-}
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }});
+
 
 promptEvent.prompt()
 window.deferredPrompt = null
